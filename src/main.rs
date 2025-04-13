71507123d0b31d2c11c08d803e821e851127c0b5
Guy Marshall <guymarshall9@gmail.com>
2025-04-13 16:59:17 +0100

    UPDATE: Move count_divisors() to separate divisors.rs file to reduce main.rs clutter

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,9 @@
+mod divisors;
+
 use std::io::{self, Write};
 
+use divisors::count_divisors;
 use thousands::Separable;
-
-fn count_divisors(mut number: i32) -> i32 {
-    let mut divisor_count: i32 = 1;
-    let mut current_divisor: i32 = 2;
-    while current_divisor * current_divisor <= number {
-        let mut exponent: i32 = 0;
-        while number % current_divisor == 0 {
-            number /= current_divisor;
-            exponent += 1;
-        }
-        if exponent > 0 {
-            divisor_count *= exponent + 1;
-        }
-        current_divisor += 1;
-    }
-    if number > 1 {
-        divisor_count *= 2;
-    }
-    divisor_count
-}
 
 fn main() {
     let mut max_divisors: i32 = 0;
